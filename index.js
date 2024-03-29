211b10705538af1416cbb472e521d15b61e46fda
ummahanakcan <star2023.06@gmail.com>
2024-03-29 15:18:39 +0300

    add javascript

---
+++ index.js
@@ -0,0 +1,92 @@
+const download = document.querySelector(".download");
+const dark = document.querySelector(".dark");
+const light = document.querySelector(".light");
+const qrContainer = document.querySelector(".qr-code");
+const qrText = document.querySelector(".qr-text");
+const shareBtn = document.querySelector(".share-btn");
+const sizes = document.querySelector(".sizes");
+
+dark.addEventListener("input", handDarkColor);
+light.addEventListener("input", handLightColor);
+qrText.addEventListener("input", handleQRText);
+sizes.addEventListener("change", handleSize);
+shareBtn.addEventListener("click", handleShare);
+
+const defaultUrl = "https://youtube.com/@AsmrProg";
+let colorLight = "#fff",
+  colorDark = "#000",
+  text = defaultUrl,
+  size = 300;
+
+function handDarkColor(e) {
+  colorDark = e.target.value;
+  generateQRCode();
+}
+
+function handLightColor(e) {
+  colorLight = e.target.value;
+  generateQRCode();
+}
+
+function handleQRText(e) {
+  const value = e.target.value;
+  text = value;
+  if (!value) {
+    qrContainer.classList.remove("active");
+    return;
+  } else {
+    qrContainer.classList.add("active");
+  }
+  generateQRCode();
+}
+
+function handleSize(e) {
+  size = e.target.value;
+  generateQRCode();
+}
+
+async function generateQRCode() {
+  qrContainer.innerHTML = "";
+  new QRCode(qrContainer, {
+    text,
+    height: size,
+    width: size,
+    colorLight,
+    colorDark,
+  });
+  download.href = await resolveDataUrl();
+}
+
+async function handleShare() {
+  setTimeout(async () => {
+    try {
+      const base64url = await resolveDataUrl();
+      const blob = await (await fetch(base64url)).blob();
+      const file = new File([blob], "QRCode.png", {
+        type: blob.type,
+      });
+      await navigator.share({
+        files: [file],
+        title: text,
+      });
+    } catch (error) {
+      alert("Your browser doesn't support sharing.");
+    }
+  }, 100);
+}
+
+function resolveDataUrl() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const img = document.querySelector(".qr-code img");
+      if (img.currentSrc) {
+        resolve(img.currentSrc);
+        return;
+      }
+      const canvas = document.querySelector("canvas");
+      resolve(canvas.toDataURL());
+    }, 50);
+  });
+}
+
+generateQRCode();
